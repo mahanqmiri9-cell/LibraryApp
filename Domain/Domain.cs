9fc844e9b6029f1a63d24b6eae59769ec4da06cb
A <A@DESKTOP-CAG7O78>
2025-09-27 02:44:23 -0700

    Add project files.

---
+++ Domain/Domain.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Entities
+{
+ 
+        public class Book
+        {
+            public int Id;
+            public string Name { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public string Publication { get; set; }
+            public string State { get; set; }
+
+
+        }
+
+        public interface IBookMethods
+        {
+             public void AddBook(Book book);
+            public Book GetBookByName(string name);
+            public List<Book> GetAllBooks(string name);
+            
+        }
+
+}
---
+++ Service/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Domain.Entities;
+
+
+namespace Service.Entities
+{
+    public class ServiceManegment : IBookMethods
+    {
+        List<Book> ServiceBook = new List<Book>();
+        void IBookMethods.AddBook(Book book)
+        {
+            ServiceBook.Add(book); 
+        }
+        Book IBookMethods.GetBookByName(string name)
+        {
+            return ServiceBook.FirstOrDefault(b => b.Name == name); 
+        }
+        List<Book> IBookMethods.GetAllBooks(string name)
+        {
+            return ServiceBook;
+        }
+    }
+}
